ddba529249d6c6f1920b9eeae0f8c987871bfb06
Rosa Rubio <paulina.rubio.p@gmail.com>
2024-11-27 11:41:10 -0300

    allowed_host

--- proyecto_inmuebles/settings.py
+++ proyecto_inmuebles/settings.py
@@ -32,10 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "desafioinmuebles-fhfzedauafdccygx.brazilsouth-01.azurewebsites.net",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
