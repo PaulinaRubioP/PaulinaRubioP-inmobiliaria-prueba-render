cc967646322bfeed0ea94f7a9a42339b6f479a6b
Rosa Rubio <paulina.rubio.p@gmail.com>
2024-11-27 12:01:34 -0300

    settings

--- proyecto_inmuebles/settings.py
+++ proyecto_inmuebles/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-ALLOWED_HOSTS = ['proyecto-inmuebles.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+#ALLOWED_HOSTS = ['proyecto-inmuebles.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
